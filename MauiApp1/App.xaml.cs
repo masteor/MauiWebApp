f912ae7b598f793b7117b70a0ac1b8fd312a307d
masteor <2064699@mail.ru>
2022-10-14 13:10:29 +0500

    Добавьте файлы проекта.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+	internal static string wwwpage = "https://www.di.fm/channels/ambient";
+
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Layouts;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Entry_Completed(object sender, EventArgs e)
+	{
+		App.wwwpage = ((Entry)sender).Text;
+		AppShell.Current.Title = ((Entry)sender).Text;
+		webView.Source = App.wwwpage;
+
+    }
+
+    // <WebView x:Name="webView" Source="https://di.fm" />
+}
+
